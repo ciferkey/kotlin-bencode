4d82dd808ba3dd767bf422ea69d93c2d5f4fccdb
Matthew Brunelle <ciferkey@gmail.com>
2018-07-29 12:22:22 -0400

    Moved encode method into sealed class

--- src/main/kotlin/com/matthewbrunelle/blog/bencode/Bencode.kt
+++ src/main/kotlin/com/matthewbrunelle/blog/bencode/Bencode.kt
@@ -2,14 +2,44 @@ package com.matthewbrunelle.blog.bencode
 
 import java.math.BigInteger
 
-sealed class Bencode
-data class BencodeString(val s: String) : Bencode()
-data class BencodeInteger(val i: BigInteger) : Bencode()
-data class BencodeList(val l: List<Bencode>) : Bencode()
-data class BencodeDict(val d: Map<Bencode, Bencode>) : Bencode()
-
 const val LIST_MARKER = 'l'
 const val INTEGER_MARKER = 'i'
 const val DICT_MARKER = 'd'
 const val TERMINATOR = 'e'
 const val SEPERATOR = ':'
+
+sealed class Bencode {
+    abstract fun encode(): String
+}
+
+data class BencodeString(val s: String) : Bencode() {
+    override fun encode(): String {
+        return s.length.toString() + s
+    }
+}
+
+data class BencodeInteger(val i: BigInteger) : Bencode() {
+    override fun encode(): String {
+        return LIST_MARKER + i.toString() + TERMINATOR
+    }
+}
+
+data class BencodeList(val l: List<Bencode>) : Bencode() {
+    override fun encode(): String {
+        return l.map { it.encode() }
+                .joinToString(SEPERATOR.toString(),
+                        prefix = LIST_MARKER.toString(),
+                        postfix = TERMINATOR.toString()
+                )
+    }
+}
+
+data class BencodeDict(val d: Map<Bencode, Bencode>) : Bencode() {
+    override fun encode(): String {
+        return d.entries.map { it.key.encode() + ":" + it.value.encode() }
+                .joinToString(SEPERATOR.toString(),
+                        prefix = LIST_MARKER.toString(),
+                        postfix = TERMINATOR.toString()
+                )
+    }
+}
